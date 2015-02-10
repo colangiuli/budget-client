f90f58ee6d8b33cac9e907a5f7f201bb9d5bd0cb
Luca Colangiuli <l.colangiuli@gmail.com>
2015-02-10 10:08:34 +0100

    no message

---
+++ www/js/controllers.js
@@ -0,0 +1,43 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('CategoriesCtrl', function($scope, Categories) {
+  $scope.categories = Categories.all();
+})
+
+.controller('CategoryDetailCtrl', function($scope, $stateParams, Categories) {
+  $scope.category = Categories.get($stateParams.categoryId);
+})
+
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
